4bc30a0ca3daea1f5adc56f9632c48bc8042301e
pirsonya <pir.sonya@list.ru>
2025-09-23 13:59:00 +0300

    m

---
+++ src/lab01/01_greeting.py
@@ -0,0 +1,4 @@
+name=input()
+age=int(input())
+print('Имя:',name, '\nВозраст:', age,'\nПривет,',name,'! Через год тебе будет',age+1)
+
---
+++ src/lab01/05_initials_and_len.py
@@ -0,0 +1,9 @@
+name=str(input())
+name_list = name.split() 
+initials = f"{name_list[0][0]}.{name_list[1][0]}.{name_list[2][0]}" 
+res=f"{initials}"
+res=res.replace('.','')
+print(res.replace('.','')) 
+print(len(name)-name.count(' ')+2)
+print('ФИО:',name,'\nИнициалы:',res.upper(),'\nДлина(символов):',len(name)-name.count(' ')+2)
+
